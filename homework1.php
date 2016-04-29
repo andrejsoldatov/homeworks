f0d374b1ae37243915ff932b4d9378b63799d118
andrejsoldatov <andrej.soldatov.88@gmail.com>
2016-04-29 22:37:30 +0300

    Added files via upload

---
+++ homework1.php
@@ -0,0 +1,13 @@
+<?php
+
+$my_age = 27;
+$name = 'Андрей Солдатов';
+$dif_age = 35-$my_age;
+if ($my_age>35){
+    echo "Меня зовут $name и мне $my_age лет";
+}
+else  {
+    echo "Меня зовут $name и до 35 лет мне осталось  лет $dif_age ";
+}
+
+?>
